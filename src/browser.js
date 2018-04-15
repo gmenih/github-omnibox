6ac574d03d14ffa41ce949f1fa523de705c85fd1
Gregor Menih <gmenih@gmail.com>
2018-04-15 17:06:55 +0200

    chore: add storage promise wrapper

--- src/browser.js
+++ src/browser.js
@@ -2,12 +2,38 @@
  * @returns {chrome}
  */
 const getBrowser = () => {
-    console.log('is chrome?', isChrome);
     if (isChrome) {
         return chrome;
     }
     return browser;
 };
+
+/** @param {chrome.storage}
+ * @returns {Storage}
+*/
+export const storageWrapper = (storage = chrome.storage.local) => {
+    return {
+        getItem(key) {
+            return new Promise((resolve) => {
+                storage.get([key], (items) => {
+                    if (!items || !items[key]) {
+                        return resolve(null);
+                    }
+                    resolve(items[key]);
+                });
+            })
+        },
+        setItem(key, value) {
+            storage.set({ [key]: value });
+        },
+        removeItem(key) {
+            storage.remove(key);
+        },
+        clear() {
+            storage.clear();
+        }
+    }
+}
 
 export const isChrome = typeof chrome !== 'undefined';
 export const browser = getBrowser();
