f47b2b7f3a55d6e4781952a02fce91761059aadf
Gregor Menih <gmenih@gmail.com>
2018-04-16 19:07:43 +0200

    refactor: use a smaller graphql library (- !120KB)

--- src/github/client.js
+++ src/github/client.js
@@ -1,64 +1,47 @@
-import { ApolloClient, ApolloError } from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import { GraphQLClient } from 'graphql-request';
+import { print } from 'graphql/language/printer';
 
 import { GITHUB_API } from '../constants';
 import queryUserDetailsGQL from './queries/queryUserOrgDetails.gql';
 import searchReposGQL from './queries/searchRepos.gql';
-
-const memCache = new InMemoryCache({
-    dataIdFromObject: object => object.id,
-    addTypename: true,
-});
 
 export class GithubClient {
     constructor(authKey) {
         if (!authKey) {
             throw new Error('Missing authKey for GithubClient');
         }
-        this.apollo = new ApolloClient({
-            link: new HttpLink({
-                uri: GITHUB_API,
-                headers: {
-                    Authorization: `Bearer ${authKey}`,
-                },
-            }),
-            cache: memCache,
+        this.client = new GraphQLClient(GITHUB_API, {
+            headers: {
+                Authorization: `Bearer ${authKey}`,
+            },
         });
     }
 
     async fetchUserLogins() {
-        const BATCH_SIZE = 2;
+        const BATCH_SIZE = 100;
         try {
             let fetchDetails = true;
             let endCursor = null;
             const allLogins = [];
             while (fetchDetails) {
-                const response = await this.apollo.query({
-                    query: queryUserDetailsGQL,
-                    variables: {
-                        after: endCursor,
-                        first: BATCH_SIZE,
-                    },
+                const response = await this.client.request(print(queryUserDetailsGQL), {
+                    after: endCursor,
+                    first: BATCH_SIZE,
                 });
-                console.log(response);
                 if (!allLogins.length) {
-                    allLogins.push(response.data.viewer.login);
+                    allLogins.push(response.viewer.login);
                 }
-                allLogins.push(...response.data.viewer.organizations.nodes.map(n => n.login));
-                if (response.data.viewer.organizations.pageInfo.endCursor
+                allLogins.push(...response.viewer.organizations.nodes.map(n => n.login));
+                if (response.viewer.organizations.pageInfo.endCursor
                     && allLogins.length >= BATCH_SIZE
                 ) {
-                    ({ endCursor } = response.data.viewer.organizations.pageInfo);
+                    ({ endCursor } = response.viewer.organizations.pageInfo);
                 } else {
                     fetchDetails = false;
                 }
             }
             return allLogins;
         } catch (err) {
-            if (err instanceof ApolloError) {
-                throw new Error(err.graphQLErrors[0].message);
-            }
             throw err;
         }
     }
@@ -69,23 +52,17 @@ export class GithubClient {
         const searchTargets = targets.map(target => `in:${target}`).join(' ');
         const searchTerm = `${text} ${searchTargets} ${userAccess} fork:true`;
         try {
-            const response = await this.apollo.query({
-                query: searchReposGQL,
-                variables: {
-                    searchTerm,
-                    first: 10,
-                },
+            const response = await this.client.request(print(searchReposGQL), {
+                searchTerm,
+                first: 10,
             });
-            const repos = response.data.search.edges;
+            const repos = response.search.edges;
             return repos.map(({ node: repo }) => ({
                 name: repo.name,
                 url: repo.url,
                 organization: repo.owner.login,
             }));
         } catch (err) {
-            if (err instanceof ApolloError) {
-                throw new Error(err.graphQLErrors[0].message);
-            }
             throw err;
         }
     }
