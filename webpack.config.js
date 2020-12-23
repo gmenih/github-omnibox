3a5206277d630283639d433173ad13eeab988354
Gregor Menih <gregor@menih.si>
2020-12-23 22:53:15 +0100

    optimizations

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 const path = require('path');
 const pkg = require('./package.json');
+const {join} = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 const MODULES_DIR = path.resolve(__dirname, './src/modules');
 
-/** 
+/**
  * @param {string} moduleName
  * @returns {[webpack.RuleSetRule]}
  */
@@ -26,13 +28,21 @@ function moduleTsRule(moduleName) {
     ];
 }
 
+const isProduction = () => process.env.NODE_ENV === 'production';
+
 /** @type {webpack.Configuration} */
 const config = {
-    devtool: 'cheap-source-map',
+    // only thing that works in a web extension
+    devtool: !isProduction() ? 'cheap-source-map' : undefined,
     entry: {
         background: path.join(__dirname, './src/modules/background/index.ts'),
         options: path.join(__dirname, './src/modules/options/index.tsx'),
         'content-script': path.join(__dirname, './src/modules/content-script/index.ts'),
+    },
+    output: {
+        path: join(__dirname, './dist'),
+        // name must also be set in `/assets/manifest.hbs`
+        filename: 'js/[name].js',
     },
     resolve: {
         extensions: ['.js', '.json', '.ts', '.tsx'],
@@ -59,7 +69,31 @@ const config = {
             },
         ],
     },
+    optimization: isProduction() ?  {
+        splitChunks: {
+            cacheGroups: {
+                core: {
+                    name: 'core',
+                    test: /[\\/]src[\\/]core[\\/]/,
+                    chunks: 'all',
+                },
+                react: {
+                    test: /[\\/]node_modules[\\/](react|react-dom|styled-components)[\\/]/,
+                    name: 'react',
+                    chunks: 'all',
+                    priority: 20,
+                },
+                vendor: {
+                    name: 'vendor',
+                    test: /[\\/]node_modules[\\/]/,
+                    chunks: 'all',
+                    priority: 10,
+                },
+            },
+        },
+    } : {},
     plugins: [
+        new CleanWebpackPlugin(),
         new DotEnvPlugin({path: './src/.env'}),
         new HtmlWebpackPlugin({
             chunks: ['options'],
